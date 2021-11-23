3491fce83a7e8a741b01968790b61e39b63c1bee
carlo <carlo.nicora@gmail.com>
2021-11-23 23:35:24 +0100

    Initial Commit

---
+++ src/Interfaces/SecurityInterface.php
@@ -0,0 +1,23 @@
+<?php
+namespace CarloNicora\Minimalism\Interfaces\Security\Interfaces;
+
+interface SecurityInterface
+{
+    /**
+     * @return SecurityInterface
+     */
+    public function getSecurityInterface(
+    ) : SecurityInterface;
+
+    /**
+     * @param string $verb
+     * @param string $uri
+     * @param array|null $body
+     * @return bool
+     */
+    public function isSignatureValid(
+        string $verb,
+        string $uri,
+        array $body=null,
+    ) : bool;
+}
